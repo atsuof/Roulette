3cf76f468f623babb158779959686950b81c07b0
Fukuda Atsuo <a.tuuus.kod.f@gmail.com>
2020-03-06 17:26:32 +0900

    first commit

---
+++ Assets/RolleteController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RolleteController : MonoBehaviour
+{
+    float _lotSpeed = 0;
+    public float lotSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lotSpeed = lotSpeed;
+        }
+        this.transform.Rotate(0, 0, this._lotSpeed);
+        this._lotSpeed *= 0.98f;
+    }
+}
